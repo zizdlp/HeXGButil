7b7715e3d86f0f33e610c36379c0140db0db1b70
zdlpsina <37832104+zdlpsina@users.noreply.github.com>
2021-08-28 12:38:20 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pybind11 import get_cmake_dir
 
 import sys
 sys.path.append("./")
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
